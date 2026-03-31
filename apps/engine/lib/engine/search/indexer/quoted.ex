6b31b4663cffa8762c67cceb7b2b755740acb414
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2026-03-31 18:07:11 +0200

    WIP

--- apps/engine/lib/engine/search/indexer/quoted.ex
+++ apps/engine/lib/engine/search/indexer/quoted.ex
@@ -1,7 +1,10 @@
 defmodule Engine.Search.Indexer.Quoted do
   alias Engine.Search.Indexer.Source.Reducer
   alias Forge.Ast.Analysis
+  alias Forge.Ast.Position
+  alias Forge.Ast.Range
   alias Forge.ProcessCache
+  alias Forge.Search.Indexer.Entry
 
   require ProcessCache
 
@@ -13,8 +16,24 @@ defmodule Engine.Search.Indexer.Quoted do
 
   def index(analysis, extractors \\ nil)
 
-  def index(%Analysis{valid?: true} = analysis, extractors) do
-    {:ok, extract_entries(analysis, extractors)}
+  def index(
+        %Analysis{valid?: true, expansions: expansions, document: document} = analysis,
+        extractors
+      ) do
+    expansion_entries =
+      Enum.flat_map(expansions, fn {range, ast} ->
+        [_ | entries] = extract_entries(Analysis.new(ast, document), extractors)
+
+        {:ok, range} = Range.from_sourceror_range(range, document)
+
+        Enum.map(entries, fn entry ->
+          %{entry | range: range}
+        end)
+      end)
+
+    regular_entries = extract_entries(analysis, extractors)
+
+    {:ok, regular_entries ++ expansion_entries}
   end
 
   def index(%Analysis{valid?: false}, _extractors) do
--- apps/forge/lib/forge/ast/range.ex
+++ apps/forge/lib/forge/ast/range.ex
@@ -8,23 +8,26 @@ defmodule Forge.Ast.Range do
 
   @spec fetch(Macro.t(), Document.t()) :: {:ok, Range.t()} | :error
   def fetch(ast, %Document{} = document) do
-    case Sourceror.get_range(ast) do
-      %{start: start_pos, end: end_pos} ->
-        [line: start_line, column: start_column] = start_pos
-        [line: end_line, column: end_column] = end_pos
-
-        range =
-          Range.new(
-            Position.new(document, start_line, start_column),
-            Position.new(document, end_line, end_column)
-          )
-
-        {:ok, range}
-
-      _ ->
-        :error
-    end
+    ast |> Sourceror.get_range() |> from_sourceror_range(document)
   end
+
+  def from_sourceror_range(
+        %Sourceror.Range{start: start_pos, end: end_pos},
+        %Document{} = document
+      ) do
+    [line: start_line, column: start_column] = start_pos
+    [line: end_line, column: end_column] = end_pos
+
+    range =
+      Range.new(
+        Position.new(document, start_line, start_column),
+        Position.new(document, end_line, end_column)
+      )
+
+    {:ok, range}
+  end
+
+  def from_sourceror_range(_, _), do: :error
 
   @spec fetch!(Macro.t(), Document.t()) :: Range.t()
   def fetch!(ast, %Document{} = document) do
